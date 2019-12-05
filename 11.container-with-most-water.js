6673fec30bb7ec27246cc28f7e1aafadc86730ce
yuchen.huang <yuchen.huang@wz-inc.com>
2019-12-05 14:33:12 +0800

    20191205

---
+++ 11.container-with-most-water.js
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=11 lang=javascript
+ *
+ * [11] Container With Most Water
+ *
+ * https://leetcode.com/problems/container-with-most-water/description/
+ *
+ * algorithms
+ * Medium (46.88%)
+ * Likes:    3989
+ * Dislikes: 476
+ * Total Accepted:    454.9K
+ * Total Submissions: 970.4K
+ * Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+ *
+ * Given n non-negative integers a1, a2, ..., an , where each represents a
+ * point at coordinate (i, ai). n vertical lines are drawn such that the two
+ * endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together
+ * with x-axis forms a container, such that the container contains the most
+ * water.
+ * 
+ * Note: You may not slant the container and n is at least 2.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In
+ * this case, the max area of water (blue section) the container can contain is
+ * 49. 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    
+};
+// @lc code=end
+
---
+++ 155.min-stack.js
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode id=155 lang=javascript
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (38.94%)
+ * Likes:    2141
+ * Dislikes: 231
+ * Total Accepted:    352.8K
+ * Total Submissions: 905.8K
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ * 
+ * 
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ * 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+// @lc code=end
+
