af09e585ffcea35eed17ca1c6e67b057ab487a93
“yuchen.huang” <yuchen.huang@wz-inc.com>
2020-01-28 15:26:56 +0800

    20200128

---
+++ 141.环形链表.js
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=javascript
+ *
+ * [141] 环形链表
+ *
+ * https://leetcode-cn.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (44.70%)
+ * Likes:    464
+ * Dislikes: 0
+ * Total Accepted:    98.8K
+ * Total Submissions: 216.5K
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * 给定一个链表，判断链表中是否有环。
+ * 
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * 
+ * 
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * 
+ * 
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 你能用 O(1)（即，常量）内存解决此问题吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    const mark = Symbol('top');
+    while(head){
+        if (head.val === mark) return true;
+        else head.val = mark;
+        head = head.next;
+    }
+    return false;
+    // const pointers = [];
+    // let result = false;
+    // const fillPointers = (node) => {
+    //     if (node && node.next !== null) {
+    //         if (pointers.includes(node.next)) result = true;
+    //         else {
+    //             pointers.push(node.next);
+    //             fillPointers(node.next);
+    //         }
+    //     }
+    // };
+    // fillPointers(head);
+    // return result;
+};
+// @lc code=end
+
--- 7.整数反转.rs
+++ 7.整数反转.rs
@@ -14,93 +14,100 @@
  * Testcase Example:  '123'
  *
  * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
- * 
+ *
  * 示例 1:
- * 
+ *
  * 输入: 123
  * 输出: 321
- * 
- * 
+ *
+ *
  * 示例 2:
- * 
+ *
  * 输入: -123
  * 输出: -321
- * 
- * 
+ *
+ *
  * 示例 3:
- * 
+ *
  * 输入: 120
  * 输出: 21
- * 
- * 
+ *
+ *
  * 注意:
- * 
+ *
  * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回
  * 0。
- * 
+ *
  */
 
 // @lc code=start
 impl Solution {
     pub fn reverse(x: i32) -> i32 {
-        pub fn check_overflow(x: i32) -> bool {
-            let max_vec: Vec<u8> = (std::i32::MAX - 1).to_string().into();
-            let max_list: Vec<char> = max_vec.into_iter().map(char::from).collect();
-        
-            let x_utf: Vec<u8> = x.to_string().into();
-            let mut number_list: Vec<char> = x_utf.into_iter().map(char::from).collect();
-            if number_list[0] == '-' {
-                number_list.remove(0);
-            } 
-        
-            if max_list.len() < number_list.len() {
-                return true;
-            } else if max_list.len() == number_list.len() {
-                for (i, digit_char) in number_list.iter().enumerate() {
-                    if digit_char.to_digit(10).unwrap() > max_list[i].to_digit(10).unwrap() {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-        if x == 1463847412 {
-            return 2147483641;
-        } else if x == -1463847412 {
-            return -2147483641;
-        } else if x == 2147483641 {
-            return 1463847412;
-        } else if x == -2147483641 {
-            return -1463847412;
-        } else if x == 0 {
-            return x;
-        }
-        if check_overflow(x) {
-            return 0;
-        }
-    
-        let x_utf: Vec<u8> = x.to_string().into();
-        let mut number_list: Vec<char> = x_utf.into_iter().map(char::from).collect();
-    
-        let mut result = Vec::new();
-        if number_list[0] == '-' {
-            result.push('-');
-            number_list.remove(0);
-        }
-        number_list.reverse();
-        let mut flag = false;
-        for digit in number_list {
-            if digit != '0' {
-                flag = true;
-            }
-            if flag {
-                result.push(digit);
-            }
-        }
-        let string: String = result.into_iter().collect();
-        let num: i32 = string.parse::<i32>().unwrap();
-        return num;
+        x.signum()
+            * x.abs()
+                .to_string()
+                .chars()
+                .rev()
+                .collect::<String>()
+                .parse::<i32>()
+                .unwrap_or(0)
+        // pub fn check_overflow(x: i32) -> bool {
+        //     let max_vec: Vec<u8> = (std::i32::MAX - 1).to_string().into();
+        //     let max_list: Vec<char> = max_vec.into_iter().map(char::from).collect();
+
+        //     let x_utf: Vec<u8> = x.to_string().into();
+        //     let mut number_list: Vec<char> = x_utf.into_iter().map(char::from).collect();
+        //     if number_list[0] == '-' {
+        //         number_list.remove(0);
+        //     }
+
+        //     if max_list.len() < number_list.len() {
+        //         return true;
+        //     } else if max_list.len() == number_list.len() {
+        //         for (i, digit_char) in number_list.iter().enumerate() {
+        //             if digit_char.to_digit(10).unwrap() > max_list[i].to_digit(10).unwrap() {
+        //                 return true;
+        //             }
+        //         }
+        //     }
+        //     return false;
+        // }
+        // if x == 1463847412 {
+        //     return 2147483641;
+        // } else if x == -1463847412 {
+        //     return -2147483641;
+        // } else if x == 2147483641 {
+        //     return 1463847412;
+        // } else if x == -2147483641 {
+        //     return -1463847412;
+        // } else if x == 0 {
+        //     return x;
+        // }
+        // if check_overflow(x) {
+        //     return 0;
+        // }
+
+        // let x_utf: Vec<u8> = x.to_string().into();
+        // let mut number_list: Vec<char> = x_utf.into_iter().map(char::from).collect();
+
+        // let mut result = Vec::new();
+        // if number_list[0] == '-' {
+        //     result.push('-');
+        //     number_list.remove(0);
+        // }
+        // number_list.reverse();
+        // let mut flag = false;
+        // for digit in number_list {
+        //     if digit != '0' {
+        //         flag = true;
+        //     }
+        //     if flag {
+        //         result.push(digit);
+        //     }
+        // }
+        // let string: String = result.into_iter().collect();
+        // let num: i32 = string.parse::<i32>().unwrap();
+        // return num;
     }
 }
 // @lc code=end
-
