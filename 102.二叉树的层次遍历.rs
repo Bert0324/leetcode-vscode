670eded54c67272ec9411e0d8b738807d776e4e4
“yuchen.huang” <yuchen.huang@wz-inc.com>
2020-01-30 14:43:47 +0800

    20200130

---
+++ 102.二叉树的层次遍历.rs
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=rust
+ *
+ * [102] 二叉树的层次遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (59.88%)
+ * Likes:    356
+ * Dislikes: 0
+ * Total Accepted:    72.4K
+ * Total Submissions: 119.7K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+ *
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ *
+ * 返回其层次遍历结果：
+ *
+ * [
+ * ⁠ [3],
+ * ⁠ [9,20],
+ * ⁠ [15,7]
+ * ]
+ *
+ *
+ */
+
+// @lc code=start
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {}
+}
+// @lc code=end
---
+++ 1104.二叉树寻路.rs
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=1104 lang=rust
+ *
+ * [1104] 二叉树寻路
+ *
+ * https://leetcode-cn.com/problems/path-in-zigzag-labelled-binary-tree/description/
+ *
+ * algorithms
+ * Medium (66.13%)
+ * Likes:    18
+ * Dislikes: 0
+ * Total Accepted:    2.5K
+ * Total Submissions: 3.8K
+ * Testcase Example:  '14'
+ *
+ * 在一棵无限的二叉树上，每个节点都有两个子节点，树中的节点 逐行 依次按 “之” 字形进行标记。
+ *
+ * 如下图所示，在奇数行（即，第一行、第三行、第五行……）中，按从左到右的顺序进行标记；
+ *
+ * 而偶数行（即，第二行、第四行、第六行……）中，按从右到左的顺序进行标记。
+ *
+ *
+ *
+ * 给你树上某一个节点的标号 label，请你返回从根节点到该标号为 label 节点的路径，该路径是由途经的节点标号所组成的。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：label = 14
+ * 输出：[1,3,4,14]
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：label = 26
+ * 输出：[1,2,6,10,26]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= label <= 10^6
+ *
+ *
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn path_in_zig_zag_tree(label: i32) -> Vec<i32> {}
+}
+// @lc code=end
