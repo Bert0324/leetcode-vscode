bdf22696b6d2ccd99bf09a87df70929ce713fd8c
“yuchen.huang” <yuchen.huang@wz-inc.com>
2020-01-30 20:57:58 +0800

    20200130

--- 102.二叉树的层次遍历.rs
+++ 102.二叉树的层次遍历.rs
@@ -59,19 +59,34 @@ use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
     pub fn level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
-        let mut container: Vec<Vec<i32>> = vec![];
+        use std::collections::VecDeque;
+
+        let mut ret = Vec::new();
         if root.is_none() {
-            return container;
+            return ret;
         }
-        Self::fill_vec(root, &mut container)
-    }
-
-    fn fill_level_vec(node: Option<Rc<RefCell<TreeNode>>>, list: &mut Vec<i32>) -> Vec<i32> {
-        vec![]
-    }
-
-    fn fill_vec(root: Option<Rc<RefCell<TreeNode>>>, list: &mut Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        vec![]
+        let mut sub_vec = Vec::new();
+        let mut queue = VecDeque::new();
+        let mut cur_level = 0;
+        queue.push_back((0, root.clone()));
+        while !queue.is_empty() {
+            if let Some((lev, Some(node))) = queue.pop_front() {
+                if lev > cur_level {
+                    ret.push(sub_vec.clone());
+                    sub_vec.clear();
+                    cur_level = lev;
+                }
+                sub_vec.push(node.borrow().val);
+                queue.push_back((lev + 1, node.borrow().left.clone()));
+                queue.push_back((lev + 1, node.borrow().right.clone()));
+            }
+        }
+    
+        if !sub_vec.is_empty() {
+            ret.push(sub_vec);
+        }
+    
+        ret
     }
 }
 // @lc code=end
