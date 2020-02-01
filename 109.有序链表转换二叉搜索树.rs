232c9501eadff5b5cd5e94135b16eb1cb0fa7007
“yuchen.huang” <yuchen.huang@wz-inc.com>
2020-02-01 21:45:50 +0800

    20200201

---
+++ 109.有序链表转换二叉搜索树.rs
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=109 lang=rust
+ *
+ * [109] 有序链表转换二叉搜索树
+ *
+ * https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (69.27%)
+ * Likes:    136
+ * Dislikes: 0
+ * Total Accepted:    17.1K
+ * Total Submissions: 24.4K
+ * Testcase Example:  '[-10,-3,0,5,9]'
+ *
+ * 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+ *
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+ *
+ * 示例:
+ *
+ * 给定的有序链表： [-10, -3, 0, 5, 9],
+ *
+ * 一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+ *
+ * ⁠     0
+ * ⁠    / \
+ * ⁠  -3   9
+ * ⁠  /   /
+ * ⁠-10  5
+ *
+ *
+ */
+
+// @lc code=start
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn sorted_list_to_bst(head: Option<Box<ListNode>>) -> Option<Rc<RefCell<TreeNode>>> {
+        
+    }
+}
+// @lc code=end
