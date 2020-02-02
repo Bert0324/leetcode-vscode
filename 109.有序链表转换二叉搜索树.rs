c8981581a07cc646389916cb75f85d8757ac9956
“yuchen.huang” <yuchen.huang@wz-inc.com>
2020-02-02 17:47:55 +0800

    20200202

--- 109.有序链表转换二叉搜索树.rs
+++ 109.有序链表转换二叉搜索树.rs
@@ -71,7 +71,26 @@ use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
     pub fn sorted_list_to_bst(head: Option<Box<ListNode>>) -> Option<Rc<RefCell<TreeNode>>> {
-        
+        let mut ptr = &head;
+        let mut cnt = 0;
+        while let Some(boxed) = ptr {
+            cnt += 1;
+            ptr = &boxed.next 
+        }
+        let mut ptr = &head;
+        Self::_list_to_bst(&mut ptr, cnt)
+    }
+    fn _list_to_bst(ptr: &mut &Option<Box<ListNode>>, len: usize) -> Option<Rc<RefCell<TreeNode>>> {
+        if len == 0 {
+            return None;
+        }
+        let left = Self::_list_to_bst(ptr, len / 2);
+        let boxed = ptr.as_ref().unwrap();
+        let mut node = TreeNode::new(boxed.val);
+        node.left = left;
+        *ptr = &boxed.next;
+        node.right = Self::_list_to_bst(ptr, len - len / 2 - 1);
+        Some(Rc::new(RefCell::new(node)))
     }
 }
 // @lc code=end
